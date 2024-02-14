dd8f0c6d71d412f4c0d46da34192c6bca55cfffe
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-02-14 12:58:48 -0600

    Made changes to App in src folder

--- src/App.js
+++ src/App.js
@@ -1,112 +1,20 @@
-import { useState, useEffect } from 'react'
-import TodoList from './components/TodoList/TodoList'
-import styles from './App.module.scss'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Bookmark from './pages/Bookmark/Bookmark'
+import About from './pages/About/About'
+import Nav from './components/Nav/Nav'
 
 export default function App(){
-    const [todos, setTodos] = useState([])
-    const [completedTodos, setCompletedTodos] = useState([])
-    const [newTodo, setNewTodo] = useState({
-        title: '',
-        completed: false
-    })
-
-    //createTodos
-    const createTodo = async () => {
-        const body = {...newTodo}
-        try {
-            const response = await fetch('/api/todos', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(body)
-            })
-            const createdTodo = await response.json()
-            const todosCopy = [createdTodo,...todos]
-            setTodos(todosCopy)
-            setNewTodo({
-                title: '',
-                completed: false
-            })
-        } catch (error) {   
-            console.error(error)
-        }
-    }
-    //deleteTodos
-    const deleteTodo = async (id) => {
-        try {
-            const index = completedTodos.findIndex((todo) => todo._id === id)
-            const completedTodosCopy = [...completedTodos]
-            const response = await fetch(`/api/todos/${id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            await response.json()
-            completedTodosCopy.splice(index, 1)
-            setCompletedTodos(completedTodosCopy)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    //moveToCompleted
-    const moveToCompleted = async (id) => {
-        try {
-            const index = todos.findIndex((todo) => todo._id === id)
-            const todosCopy = [...todos]
-            const subject = todosCopy[index]
-            subject.completed = true 
-            const response = await fetch(`/api/todos/${id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(subject)
-            })
-            const updatedTodo = await response.json()
-            const completedTDsCopy = [updatedTodo, ...completedTodos]
-            setCompletedTodos(completedTDsCopy)
-            todosCopy.splice(index, 1)
-            setTodos(todosCopy)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    //getTodos
-    const getTodos = async () => {
-        try{
-            const response = await fetch('/api/todos')
-            const foundTodos = await response.json()
-            setTodos(foundTodos.reverse())
-            console.log('hey')
-            const responseTwo = await fetch('/api/todos/completed')
-            const foundCompletedTodos = await responseTwo.json()
-            setCompletedTodos(foundCompletedTodos.reverse())
-        } catch(error){
-            console.error(error)
-        }
-    }
-    useEffect(() => {
-        getTodos()
-    }, [])
-    return(
-        <>
-			
-            <div className={styles.banner}>
-                <h1>The World Famous Big Poppa Code React Starter Kit</h1>
-              <img src='https://i.imgur.com/5WXigZL.jpg'/>
-            </div>
-            <TodoList
-            newTodo={newTodo}
-            setNewTodo={setNewTodo}
-            createTodo={createTodo}
-            todos={todos}
-            moveToCompleted={moveToCompleted}
-            completedTodos={completedTodos}
-            deleteTodo={deleteTodo}
-            />
-        </>
+    return (
+        <div className='App'>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>      
+                <Route path="/about" element={<About/>}/>      
+                <Route path="/dashboard" element={<Dashboard/>}/>      
+                <Route path="/bookmarks/:symbol" element={<Bookmark/>}/>                                                
+            </Routes>
+        </div>
     )
 }
