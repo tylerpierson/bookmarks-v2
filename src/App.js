830bfd9a896c08221816993391047ba11d40145d
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-02-14 12:59:43 -0600

    Deleted imports from App in src folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Bookmark from './pages/Bookmark/Bookmark'
-import About from './pages/About/About'
-import Nav from './components/Nav/Nav'
+
 
 export default function App(){
     return (
         <div className='App'>
-            <Nav/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>      
-                <Route path="/about" element={<About/>}/>      
-                <Route path="/dashboard" element={<Dashboard/>}/>      
-                <Route path="/bookmarks/:symbol" element={<Bookmark/>}/>                                                
-            </Routes>
+                   
         </div>
     )
 }
