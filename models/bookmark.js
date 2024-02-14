55d73727134a0c52d4f958255dbaafbf816d41eb
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-02-14 12:43:52 -0600

    Changed the bookmark schema

---
+++ models/bookmark.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require('mongoose')
+
+
+const bookmarkSchema = new Schema ({
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+})
+
+const Bookmark = model('Bookmark', bookmarkSchema)
+
+module.exports = Bookmark
