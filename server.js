b255a3da6b1b5d2852e8afba145bbd853f6f027d
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-02-14 12:56:41 -0600

    Made changes to webpack config file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = require('./app-server')
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-	console.log(`I am listening on ${PORT}. We in the Building`)
+	console.log(`I am listening on ${PORT}.`)
 })
 
 
